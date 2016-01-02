439e5c6f0eed7be582946745c94d657e5976e410
lulukas <lulukas.klier@gmail.com>
2016-01-02 12:43:34 +0100

    some changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -162,3 +162,15 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
 gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
+
+var gutil = require('gulp-util');
+var ftp = require('gulp-ftp');
+
+gulp.task('deploy', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe(ftp({
+      host: 'ftp.rekli.net',
+      user: 'lukas@rekli.net',
+      pass: 'lukas.klier@rekli.net',
+      remotePath: '/hong-kong-blog'
+  })).pipe(gutil.noop());
+});
